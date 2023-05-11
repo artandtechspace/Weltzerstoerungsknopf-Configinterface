49b3c2aaa77fe572bec020450d7813b26b6f9b0b
Noah Albers <noah.albers@artandtech.space>
2023-05-11 18:15:25 +0200

    - Added first version

--- src/core/logsystem/LogType.ts
+++
@@ -1,7 +0,0 @@
-export enum LogType {
-    ERROR = "error",
-    DEBUG = "debug",
-    WARNING = "warn",
-    INFO = "info",
-    STATE_SWITCH = "state"
-}
--- src/utils/VueUtils.ts
+++
@@ -1,4 +0,0 @@
-// Returns a reference from a vue-component
-export function getRef<T>(base: any, name: string) : T{
-    return base.$refs[name] as T;
-}
